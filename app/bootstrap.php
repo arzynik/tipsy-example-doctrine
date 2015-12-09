c8ee3cfa5c694d5a2c969a4d50a8348bad92aed4
Devin Smith <github@arzynik.com>
2015-12-09 10:34:05 -0800

    database and install fixes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,3 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 \Tipsy\Tipsy::config();
 \Tipsy\Tipsy::config(__DIR__.'/config.ini');
+
+if (getenv('DATABASE_URL')) {
+	\Tipsy\Tipsy::config([db => [url => getenv('DATABASE_URL')]]);
+}
